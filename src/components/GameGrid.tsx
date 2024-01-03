1d5f1dd701ebb398c428eecf5ef782195c9e986f
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-01-03 09:36:11 +0100

    Create a reusable hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,33 +1,9 @@
-import { useEffect, useState } from "react";
 import { Text } from "@chakra-ui/react";
-import apiClient, { CanceledError } from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-interface FetchGamesResponse {
-  results: Game[];
-  count: number;
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    const controller = new AbortController();
-    apiClient
-      .get<FetchGamesResponse>("games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err);
-      });
+  const { games, error } = useGames();
 
-    return () => {
-      controller.abort();
-    };
-  }, []);
   return (
     <>
       {error && <Text>{error}</Text>}
