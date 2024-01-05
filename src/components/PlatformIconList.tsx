11a20364a125e5f6d2f89d7b2a4c024e7b0c704b
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-01-05 18:56:35 +0100

    Display platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,46 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { Platform } from "../hooks/useGames";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconsMap: { [key: string]: IconType } = {
+    xbox: FaXbox,
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    nintendo: SiNintendo,
+  };
+
+  return (
+    <HStack>
+      {platforms.map((platform) => (
+        <Icon
+          color="gray.500"
+          marginY={"10px"}
+          as={iconsMap[platform.slug]}
+        ></Icon>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
