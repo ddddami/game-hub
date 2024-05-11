f1179ba1b3e8419164ab9bf00b752c01475593f7
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-05-11 21:52:27 +0100

    Customize the Chakra color theme to give darker gray

--- src/theme.ts
+++ src/theme.ts
@@ -5,5 +5,21 @@ const config: ThemeConfig = {
   useSystemColorMode: true,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#eef1f9",
+      100: "#d2d5df",
+      200: "#b5b9c7",
+      300: "#979db1",
+      400: "#7a809b",
+      500: "#616782",
+      600: "#4b5066",
+      700: "#363949",
+      800: "#20222d",
+      900: "#0b0b14",
+    },
+  },
+});
 export default theme;
